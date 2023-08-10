31098b6bac657b0f1370e9e010398fb12ca699f6
SULPHURIC4CID <108265852+SULPHURIC4CID@users.noreply.github.com>
2023-08-10 22:48:23 +0530

    Add files via upload

---
+++ Strings/src/Question2.java
@@ -0,0 +1,22 @@
+/*
+
+Author : Ananthanarayanan R
+Batch: Uttara B107
+Question: 2
+
+Description:
+
+How can you find the length of a string in Java?
+*/
+
+public class Question2
+{
+	
+	public static void main(String[] args)
+	{
+		System.out.println();
+		String s = "Anantha";
+		System.out.println("Length of String \""+s+"\" is : "+s.length());
+		
+	}
+}
